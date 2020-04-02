a814889ded51b8eef21268a4c8c84aef067732f1
Dmitry Rakov <rakov.di@gmail.com>
2020-04-02 22:48:43 +0300

    [+] Добавляет gulp для минификации css/js. Минифицирует js

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+// =======================
+// Используемые npm-пакеты
+// Общие
+// =======================
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+
+const cssnano = require('gulp-cssnano');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+
+
+// ==========
+// Base tasks
+// ==========
+
+// Build css styles
+gulp.task('css', () => {
+  return gulp.src(['./css/*.css','!./css/*.min.css'])
+    .pipe(autoprefixer('>1% and last 2 versions and not IE 11'))
+    .pipe(cssnano())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./css/'));
+});
+
+// Build scripts
+gulp.task('js', () => {
+  return gulp.src(['./js/*.js','!./js/*.min.js','!./js/multimedia.js'])
+    .pipe(babel({
+      presets: [
+        ['@babel/env', {
+          modules: false
+        }]
+      ]
+    }))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./js'));
+});
+
+// ==========
+// Composed tasks
+// ==========
+gulp.task('build', gulp.series( 'css', 'js'));
---
+++ js/banner.min.js
@@ -0,0 +1,1 @@
+document.write('<a target=_blank href="http://ya.ru"></a>');
