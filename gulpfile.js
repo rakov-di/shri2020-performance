1ff7b7afa80d6ea9420c29c88b18fbac2f6e536f
Dmitry Rakov <rakov.di@gmail.com>
2020-04-12 14:34:45 +0300

    [*] Инлайнит стили в html

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const rename = require('gulp-rename');
 
 const cssnano = require('gulp-cssnano');
 const autoprefixer = require('gulp-autoprefixer');
+const concatCss = require('gulp-concat-css');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
 
@@ -19,6 +20,18 @@ const babel = require('gulp-babel');
 gulp.task('css', () => {
   return gulp.src(['./css/*.css','!./css/*.min.css'])
     .pipe(autoprefixer('>1% and last 2 versions and not IE 11'))
+    .pipe(cssnano())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./css/'));
+});
+
+// Build css styles
+gulp.task('css_all', () => {
+  return gulp.src(['./css/default.css','./css/main.css'])
+    .pipe(autoprefixer('>1% and last 2 versions and not IE 11'))
+    .pipe(concatCss('all.css'))
     .pipe(cssnano())
     .pipe(rename({
       suffix: '.min'
